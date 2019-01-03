069d8e01b1d7998f7ed6592c241b78cc016d16ef
logan3421 <46356282+logan3421@users.noreply.github.com>
2019-01-03 19:32:29 +0000

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,4 +4,4 @@ bot.on("ready", function()  {
  bot.user.setGame("HELLO")
     console.log("LE");
 });
-bot.login("NTMwNDUzNzk2MTU3OTE1MTUx.Dw_nqw.CSh0bltlMAYAyYGe9LBBn6gARIQ");
+bot.login("NTMwNDUzNzk2MTU3OTE1MTUx.Dw_0rg.Cmqc7cmaud-ZepLDgTAihxftyHA");
